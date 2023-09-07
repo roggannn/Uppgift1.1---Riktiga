668717a6609650395ade64306e57454f4b4158ac
roggannn <144336997+roggannn@users.noreply.github.com>
2023-09-07 13:21:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Uppgift1_1
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1."); //Nästa rad av kod kommer att skriva ut en text
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\"); //Nästa rad kommer att "läsa" din nästa knapp-tryckning
+            Console.ReadKey();
+        }
+
+    }
+}
